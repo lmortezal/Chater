3229c71488ae490d67177884794c60315bed8693
morteza <lmortezal.world@gmail.com>
2024-02-20 23:34:10 +0330

    just send and print data

--- client/client.go
+++ client/client.go
@@ -1,3 +1,4 @@
+
 package client
 
 import (
@@ -5,19 +6,26 @@ import (
 	"fmt"
 )
 
-
-func Startconnection(ip string , domain int){
-	fmt.Println("1")
-
-	conn , err := net.Dial("tcp4" , "localhost:8081")
-
-	data := []byte("Hello, Server!")
-	_, err = conn.Write(data)
+func Startconnection(domain string , port int){
+	// connect to this socket
+	var addr = fmt.Sprintf("%s:%d", domain, port)
+	var msg string  
+	conn, err := net.Dial("tcp", addr)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error:", err.Error())
 		return
 	}
-	 
+
+	fmt.Printf("connecting to %v :\nNow you can type it !\n" , addr)
+	for {
+		if err != nil {
+			// handle error
+			fmt.Println("Error:", err.Error())
+		}
+		fmt.Scan(&msg)
+		fmt.Fprintf(conn, msg + "\n")
+	}
+	
 
 }
 	
