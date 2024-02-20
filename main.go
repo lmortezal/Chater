ffcf5e02a546f6679911fd35f5d3a2cac0ae75af
morteza <lmortezal.world@gmail.com>
2024-02-20 23:35:14 +0330

    add client side in main

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/lmortezal/ChatOnline/cmd"
 	s "github.com/lmortezal/ChatOnline/server"
+	"github.com/lmortezal/ChatOnline/client"
 )
 
 func main() {
 	domain, port, server := cmd.Execute()
 	if server {
 		s.Startlistening(domain, port)
+	} else if !server{
+		client.Startconnection(domain, port)
 	}
 }
