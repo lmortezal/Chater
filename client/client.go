a5d9fc8200e07be641379343b78b86f7ba03fbdf
morteza <lmortezal.world@gmail.com>
2024-02-23 00:32:31 +0330

    Code cleanup

--- client/client.go
+++ client/client.go
@@ -3,7 +3,6 @@ package client
 import (
 	"bufio"
 	"fmt"
-	// "net"
 	"os"
 	"crypto/tls"
 )
@@ -17,18 +16,10 @@ func Startconnection(domain string , port int){
 
 	// connect to this socket
 	var addr = fmt.Sprintf("%s:%d", domain, port)
-	// for i := 1; i < 5 ; i++{
-	// 	//time.Sleep(time.Duration(i * 10) * time.Second)
-	// 	for j := i * 10; j > 0 ; j--{
-	// 		fmt.Printf("Trying to connect to %v in %d Seconds\r",  addr ,j)
-	// 		time.Sleep(time.Second) // Adjust the duration as needed
-	// 	}
-	// 	fmt.Println()
-	// 	fmt.Print("\033[H\033[2J")
-	// }
-	conn_tls, err2 := tls.Dial("tcp", addr, &tls.Config{InsecureSkipVerify: true} )
-	//conn, err := net.Dial("tcp", addr)
+	//TODO // add check connection to the server 
 
+
+	conn_tls, err2 := tls.Dial("tcp", addr, &tls.Config{InsecureSkipVerify: true} )
 	if err2 != nil {
 		fmt.Println("Error:", err2.Error())
 		return
--- server/server-side.go
+++ server/server-side.go
@@ -34,7 +34,6 @@ func Startlistening(domain string, port int) {
 	if port == 0 {
 		port = 8080
 	}
-	port_tls := port + 1
 	cert , errFile_cert := tls.LoadX509KeyPair("./server/server.crt", "./server/server.key")
 	if errFile_cert != nil{
 		fmt.Println(errFile_cert.Error())
@@ -42,24 +41,16 @@ func Startlistening(domain string, port int) {
 	config_tls := tls.Config{Certificates: []tls.Certificate{cert}}
 
 	var addr = fmt.Sprintf("%s:%d", domain, port)
-	var addr_tls = fmt.Sprintf("%s:%d", domain, port_tls)
-	l, err := net.Listen("tcp4", addr)
-	l_tld , err2 := tls.Listen("tcp4", addr_tls, &config_tls)
-
-	if err2 != nil {
-		log.Fatal(err2)
-	} else{
-		fmt.Println("Listening on ", addr)
-	}
+	l_tld , err := tls.Listen("tcp4", addr, &config_tls)
 
 	if err != nil {
 		log.Fatal(err)
 	} else{
-		fmt.Println("Listening on ", addr_tls)
+		fmt.Println("Listening on ", addr)
 	}
+
 	// Close the listener when the application closes. and handle the error 
 	// each client send to server give it and print in the server and return to the client
-	defer l.Close()
 	defer l_tld.Close()
 
 
@@ -74,11 +65,9 @@ func Startlistening(domain string, port int) {
 			log.Fatal(err2)
 			log.Fatal(err)
 		}
-		fmt.Println(conn_tld)
+		
 		// Handle connections in a new goroutine.
-		// clients[Client{conn, ""}] = true // this is a bug
-		//go handleRequest(conn)
-		go handleRequestTls(conn_tld)
+		go handleRequest(conn_tld)
 		
 
 	}
@@ -95,28 +84,8 @@ func Boradcast(){
 			}
 		}
 }
-func handleRequestTls(conn net.Conn){
-	input := bufio.NewScanner(conn)
-	fmt.Println("New connection from:", conn.RemoteAddr())
-	var name string
-	for input.Scan(){
-		name = input.Text()
-		break
-	}
-	
-	cli := Client{conn, name}
-	clients[cli] = true
-
-	for input.Scan(){
-		if input.Text() == ""{
-			continue
-		}
-		messages <- name + " say : " + input.Text()
-		fmt.Println("Received from ", name, ":", input.Text())
-	}
 
 
-}
 // Handles incoming requests.
 func handleRequest(conn net.Conn) {
 	input := bufio.NewScanner(conn)
